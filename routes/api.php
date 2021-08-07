4566af0fcb44bb30a1276c4c7f3c72cc96717d1c
UsamaHussain000111 <usamahussaincs@gmail.com>
2021-08-07 08:33:15 -0700

    got stuck at tokens(), in AuthController

---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+// public routes
+
+//Route::resource('products' , ProductsController::class);
+
+
+Route::post('/register' , [AuthController::class , 'register']);
+Route::get('/products' , [ProductsController::class, 'index']);
+Route::get('/products/{id}' , [ProductsController::class, 'show']);
+Route::get('/products/search/{name}' , [ProductsController::class, 'search']);
+
+
+// protected routes
+Route::group([ 'middleware' => ['auth:sanctum']] , function () {
+    Route::post('/products' , [ProductsController::class , 'store']);
+    Route::put('/products/{id}' , [ProductsController::class , 'update']);
+    Route::delete('/products/{id}' , [ProductsController::class , 'destroy']);
+    Route::post('/logout' , [AuthController::class] , 'logout');
+});
+
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
